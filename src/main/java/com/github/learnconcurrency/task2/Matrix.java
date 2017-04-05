0e77b11c2c7dcc47973f47fa287e422375579e5b
RastegaevYO <RastegaevYO@sovcombank.ru>
2017-04-05 15:05:16 +0400

    Matrix task

---
+++ src/main/java/com/github/learnconcurrency/task2/Matrix.java
@@ -0,0 +1,35 @@
+package com.github.learnconcurrency.task2;
+
+/**
+ * @author RastegaevYO
+ */
+public class Matrix {
+    private int[][] data;
+    private int[][] reversedData;
+
+    public Matrix(int[][] data) {
+        this.data = data;
+    }
+
+    public int[] getRow(int i) {
+        return data[i];
+    }
+
+    public int[] getColumn(int i) {
+        if (reversedData == null) {
+            reversedData = fillReversedMatrixData(data);
+        }
+        return reversedData[i];
+    }
+
+    private int[][] fillReversedMatrixData(int[][] data) {
+        int n = data[0].length;
+        int[][] reversedData = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                reversedData[i][j] = data[j][i];
+            }
+        }
+        return reversedData;
+    }
+}
